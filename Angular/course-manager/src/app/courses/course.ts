020bb12874d59f351e5545ad24ec4942458be9c3
Mateusp <mateus.p@gsuite.iff.edu.br>
2022-07-29 10:07:26 -0300

    Projeto em angular - fist commit

---
+++ Angular/course-manager/src/app/courses/course.ts
@@ -0,0 +1,11 @@
+export class Course { 
+    id: number = 1;
+    name: string = 'nome';
+    imageUrl: string = 'url';
+    price: number = 0;
+    code: string = "codigo";
+    duration: number = 0;
+    rating: number = 0;
+    releaseDate: string = 'data';
+    description: string = 'Description'; 
+}
