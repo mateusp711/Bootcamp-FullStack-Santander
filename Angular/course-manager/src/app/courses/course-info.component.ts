6defe30437fd78fab9a73cb56a6824389cb62e45
Mateusp <mateus.p@gsuite.iff.edu.br>
2022-08-01 12:46:26 -0300

    mudanças no tsconfig e correções de código

--- Angular/course-manager/src/app/courses/course-info.component.ts
+++ Angular/course-manager/src/app/courses/course-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { Course } from "./course";
+import { CourseService } from './course.service';
 
 
 @Component({
@@ -7,12 +9,17 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class CourseInfoComponent implements OnInit {
 
-    courseId: number = 0;
+    course: Course;
 
-    constructor(private activatedRoute: ActivatedRoute) { }
+    constructor(private activatedRoute: ActivatedRoute, private courseService: CourseService) { }
 
     ngOnInit(): void {
-        this.courseId = +!this.activatedRoute.snapshot.paramMap.get('id'); 
+        this.course = this.courseService.retrieveById(+this.activatedRoute.snapshot.paramMap.get('id')); 
+    }
+
+    save(): void {
+        this.courseService.save(this.course);
+
     }
 
 }
