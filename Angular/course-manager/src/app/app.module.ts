019b67bf31fe88832f8528990f4ace934c9e60b9
Mateusp <mateus.p@gsuite.iff.edu.br>
2022-08-03 21:21:43 -0300

    Aplicando tratamento de erros

--- Angular/course-manager/src/app/app.module.ts
+++ Angular/course-manager/src/app/app.module.ts
@@ -3,10 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@Angular/forms';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { Error404Component } from './erro404/error-404.component';
 import { courseModule } from './courses/course.module';
 import { CoreModule } from './core/core.module';
 
@@ -15,7 +13,6 @@ import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component
     
 
   
@@ -27,8 +24,7 @@ import { CoreModule } from './core/core.module';
     courseModule,
     CoreModule,
     RouterModule.forRoot([
-      {path: '', redirectTo: 'courses', pathMatch: 'full'},
-      {path: '**', component: Error404Component}
+      {path: '', redirectTo: 'courses', pathMatch: 'full'}
     ])
   ],
   providers: [],
--- Angular/course-manager/src/app/core/core.module.ts
+++ Angular/course-manager/src/app/core/core.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { Error404Component } from "./component/erro404/error-404.component";
 import { NavBarComponent } from "./component/nav-bar/nav-bar.component";
 
 @NgModule({
     declarations: [
         NavBarComponent,
+        Error404Component
     ],
     imports: [
-        RouterModule
+        RouterModule.forChild([
+                {path: '**', component: Error404Component}
+        ])
     ],
     exports: [
         NavBarComponent,
