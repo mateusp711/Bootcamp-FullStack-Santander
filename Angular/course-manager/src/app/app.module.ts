651e1153534e68543d097b866cae8e92345cabc9
Mateusp <mateus.p@gsuite.iff.edu.br>
2022-08-03 21:15:50 -0300

    Transferindo modulos não génericos para pasta core

--- Angular/course-manager/src/app/app.module.ts
+++ Angular/course-manager/src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@Angular/forms';
 
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { Error404Component } from './erro404/error-404.component';
 import { courseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component
     
 
@@ -25,6 +25,7 @@ import { courseModule } from './courses/course.module';
     FormsModule, 
     HttpClientModule,
     courseModule,
+    CoreModule,
     RouterModule.forRoot([
       {path: '', redirectTo: 'courses', pathMatch: 'full'},
       {path: '**', component: Error404Component}
